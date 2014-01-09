8a80260b07f01ec4a32245695b995afe42093192
Jamie <jamie@tidepool.org>
2014-01-09 16:28:12 +1300

    clean up message form

--- test/unit/MessageForm_Test.js
+++ test/unit/MessageForm_Test.js
@@ -4,12 +4,16 @@ var React = require('react');
 
 var MessageForm = require('../../build/components/MessageForm');
 
+var getSubmittedMessage = function(text){
+    console.log('added: ',text)
+};
+
 describe('MessageForm', function() {
     var component, container;
 
     beforeEach(function() {
         //we add our component to test into a div and then render it
-        component = MessageForm();
+        component = MessageForm({onMessageSubmit:getSubmittedMessage});
         container = document.createElement('div');
         document.documentElement.appendChild(container);
         React.renderComponent(component, container);
@@ -25,33 +29,18 @@ describe('MessageForm', function() {
         expect(component.refs).to.exist;
     });
 
-    it('should return new message button by default', function() {
-        var newBtn = component.refs.newBtn;    
-        expect(newBtn).to.exist;
-    });
-
-    it('should return send button when in adding state', function() {
-        component.setState({adding: true})
+    it('should return send button', function() {
         var sendBtn = component.refs.sendBtn;
         expect(sendBtn).to.exist;
     });
 
-    it('should return message text when in adding state', function() {
-        component.setState({adding: true})
+    it('should return message text', function() {
         var messageText = component.refs.messageText;
         expect(messageText).to.exist;
     });
 
-    it('should return false when we handle the submit', function() {
+    it('should return false when we add a new message', function() {
         expect(component.handleSubmit()).to.be.false;
     });
 
-    it('should return false when we handle a new message', function() {
-        expect(component.handleNewMessage()).to.be.false;
-    });
-
-    it('should have a property that exposes the message submisson', function() {
-        var onMessageSubmit = component.props.onMessageSubmit;
-        expect(onMessageSubmit).to.exist;
-    });
 });
